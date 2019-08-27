c2ad723c1ca038718752821889973b0fc8eb043c
ckopecky <kopecky12112@gmail.com>
2019-08-27 14:52:08 -0700

    getUsersByDept written

--- controller/userController.js
+++ controller/userController.js
@@ -26,11 +26,26 @@ const getUserById = (req, res) => {
         });
 };
 
+const getUsersByDepartment = (req, res) => {
+    const { department } = req.params;
+    let division = department === "cs" ? "Computer Science" : department === "fs" ? "Web Development" : department === "ds" ? "Data Science" : department === "ios" ? "iOS" : department === "ux" || department === "ui" ? "UX": department === "labs" ? "Labs" : department === "x" ? "X" : "Android";
+    Users.findBy({department: division})
+        .then(response => {
+            console.log(department, response, "hello");
+            helper.sendStatus(200, response, res);
+        })
+        .catch(err => {
+            helper.sendStatus(500, {Error: err.message}, res);
+        })
+}
+
 
 router.route('/')
     .get(getUsers)
 router.route('/:id')
     .get(getUserById)
+router.route('/department/:department')
+    .get(getUsersByDepartment);
 
 
 
